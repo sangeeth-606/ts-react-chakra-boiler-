72b648827ec3c76ebb7e83e0a63329fc6c1c3855
sangeeth911 <sangeeth999123@gmail.com>
2024-12-10 17:18:56 +0530

    fresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { ChakraProvider } from '@chakra-ui/react';
-import { Button } from '@chakra-ui/react';
-// import theme from './theme/theme';
+
+import theme from './theme/theme';
 
 function App() {
   return (
-    <ChakraProvider >
-      <Button>Click me</Button>
+    <ChakraProvider theme={theme}>
+      
       
     </ChakraProvider>
   );
